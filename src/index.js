16cfc94fdc27533eb8f310d69d7bd849791715d5
Cerdtruir <ghoornabeel@gmail.com>
2022-03-01 14:38:50 +0200

    Add test list object

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import _ from 'lodash';
 import './style.css';
+
+let todoItems = [
+  { index: 1, description: 'Eat Lunch', completed: false },
+  { index: 0, description: 'Eat Breakfast', completed: false },
+  { index: 2, description: 'Eat Supper', completed: false },
+];
