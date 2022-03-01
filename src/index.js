8c6a03e2c249458a751a147dcdacbc88f7de713c
Cerdtruir <ghoornabeel@gmail.com>
2022-03-01 14:39:29 +0200

    Add function to generate tasks in order of index

--- src/index.js
+++ src/index.js
@@ -6,3 +6,18 @@ let todoItems = [
   { index: 0, description: 'Eat Breakfast', completed: false },
   { index: 2, description: 'Eat Supper', completed: false },
 ];
+
+let currentIndex = 0;
+for (let i = 0; i < todoItems.length; i++) {
+  const task = todoItems[i];
+  if (task.index === currentIndex) {
+    let content = document.createElement('li');
+    content.innerHTML = `<span class="checkbox">&#9634;</span> ${task.description} <span class="list-span list-move">&#xFE19;</span>`;
+    content.classList.add('list-item');
+    let lastItem = document.body.querySelector('.last-item');
+    let listMain = document.body.querySelector('.todo-list');
+    listMain.insertBefore(content, lastItem);
+    currentIndex += 1;
+    i = -1;
+  }
+}
