f720f0a77ffbc97e6c26cc47830b66ccc5c3d682
Cerdtruir <ghoornabeel@gmail.com>
2022-03-01 14:45:23 +0200

    Add linters and fix errors

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
+// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
 
-let todoItems = [
+const todoItems = [
   { index: 1, description: 'Eat Lunch', completed: false },
   { index: 0, description: 'Eat Breakfast', completed: false },
   { index: 2, description: 'Eat Supper', completed: false },
 ];
 
 let currentIndex = 0;
-for (let i = 0; i < todoItems.length; i++) {
+for (let i = 0; i < todoItems.length; i += 1) {
   const task = todoItems[i];
   if (task.index === currentIndex) {
-    let content = document.createElement('li');
+    const content = document.createElement('li');
     content.innerHTML = `<span class="checkbox">&#9634;</span> ${task.description} <span class="list-span list-move">&#xFE19;</span>`;
     content.classList.add('list-item');
-    let lastItem = document.body.querySelector('.last-item');
-    let listMain = document.body.querySelector('.todo-list');
+    const lastItem = document.body.querySelector('.last-item');
+    const listMain = document.body.querySelector('.todo-list');
     listMain.insertBefore(content, lastItem);
     currentIndex += 1;
     i = -1;
