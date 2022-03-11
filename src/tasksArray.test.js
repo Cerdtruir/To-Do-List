0cc09c773a7f9a0bad2491ae1df0a7313c31a37a
Cerdtruir <ghoornabeel@gmail.com>
2022-03-11 23:23:59 +0200

    Add  assertions to test file

--- src/tasksArray.test.js
+++ src/tasksArray.test.js
@@ -4,12 +4,39 @@ import newTask, {
   removeCompleted,
 } from './tasksArray.js';
 
-describe('Test newTask', () => {
-  test('Check for correct output', () => {
-    document.body.innerHTML =
-      '<div>' +
-      '  <ul id="list"><li> <textarea placeholder="Add to your list..." class="list-text" contenteditable="true">test</textarea></li></ul>' +
-      '</div>';
+document.body.innerHTML =
+  '<div>' +
+  ' <ul id="list">' +
+  '   <li> <textarea placeholder="Add to your list..." class="list-text" contenteditable="true">test</textarea><button type="button" class="list-button">⏎</button></li>' +
+  '   <li id="0" class="list-item list-task"><p class="checkbox">▢</p> <p class="task-description" contenteditable="true">test 1</p><p> </p><p class="list-span list-move">︙</p></li>' +
+  '   <li id="1" class="list-item list-task"><p class="checkbox">▢</p> <p class="task-description" contenteditable="true">test 2</p><p> </p><p class="list-span list-move">︙</p></li>' +
+  '   <li id="2" class="list-item list-task"><p class="checkbox">▢</p> <p class="task-description" contenteditable="true">test 3</p><p> </p><p class="list-span list-move">︙</p></li>' +
+  ' </ul>' +
+  '</div>';
+
+let array = [
+  {
+    index: 0,
+    description: 'test 1',
+    completed: false,
+  },
+  {
+    index: 1,
+    description: 'test 2',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'test 3',
+    completed: false,
+  },
+];
+
+function MocktasksHTML() {
+  return 'tasksHtml';
+}
+
+const event = new KeyboardEvent('keydown', { keyCode: 13 });
     expect(newTask(1)).toStrictEqual({
       index: 1,
       description: 'test',
