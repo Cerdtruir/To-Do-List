1b6f0b59c999587251640d39730a7dcfcaf5a0a9
Cerdtruir <ghoornabeel@gmail.com>
2022-05-18 18:15:53 +0200

    Fix formatting of code

--- src/completed.js
+++ src/completed.js
@@ -1,8 +1,7 @@
 export default function checkbox(todoItems, tasksHTML) {
   document.body.querySelectorAll('.checkbox').forEach((box) => {
     box.onclick = function () {
-      todoItems[box.parentElement.id].completed =
-        !todoItems[box.parentElement.id].completed;
+      todoItems[box.parentElement.id].completed = !todoItems[box.parentElement.id].completed;
       tasksHTML();
     };
   });
